1abd060ee1bf5a9afb06b0074c903c1879247a34
Alexander Zobnin <alexanderzobnin@gmail.com>
2020-12-16 14:10:57 +0300

    apply patch

--- sync-changes.sh
+++ sync-changes.sh
@@ -21,3 +21,12 @@ node ./scripts/sync-frontend.js
 git add .
 echo "Creating a patch"
 git diff --cached > sync-patch-${BASE_COMMIT}.patch
+
+echo "Cleaning up and applying patch"
+git reset HEAD src/
+git restore src/
+git checkout sync-upstream
+git apply --3way sync-patch-${BASE_COMMIT}.patch
+git reset HEAD src/
+
+echo "Patch applied. Now you can review changes and apply it manually if patch failed."
