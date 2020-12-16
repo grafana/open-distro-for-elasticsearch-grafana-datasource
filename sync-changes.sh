0b131ecd2bb7b8d25065637ba7fbbc42b0aa5a89
Alexander Zobnin <alexanderzobnin@gmail.com>
2020-12-16 12:51:18 +0300

    update sync script

--- sync-changes.sh
+++ sync-changes.sh
@@ -2,5 +2,13 @@
 
 BASE_COMMIT=$(cat .upstream.lock)
 
-git checkout -B sync-upstream
+echo "Creating new branch"
+git checkout -B sync-upstream-${BASE_COMMIT}
+
+echo "Syncing changes with base commit"
+node ./scripts/sync-frontend.js -r ${BASE_COMMIT}
+git add .
+git commit -m "Sync changes with commit ${BASE_COMMIT}"
+
+echo "Syncing changes with master"
 node ./scripts/sync-frontend.js
