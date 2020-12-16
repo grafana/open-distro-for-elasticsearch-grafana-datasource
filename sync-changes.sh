3ff6eaf048903fe684adf2166a3c48c59aa8da31
Alexander Zobnin <alexanderzobnin@gmail.com>
2020-12-16 13:43:14 +0300

    create patch

--- sync-changes.sh
+++ sync-changes.sh
@@ -12,8 +12,12 @@ node ./scripts/sync-frontend.js -r ${BASE_COMMIT}
 git add .
 git commit -m "Sync changes with commit ${BASE_COMMIT}"
 
-echo "Merging changes"
+echo "Rebasing changes"
 git rebase sync-upstream-${BASE_COMMIT} sync-upstream
 
 echo "Syncing changes with master"
 node ./scripts/sync-frontend.js
+
+git add .
+echo "Creating a patch"
+git diff --cached > sync-patch-${BASE_COMMIT}.patch
